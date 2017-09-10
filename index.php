41b85b6d1699f27fa8d9e54fbd6e80fa36af2f33
teahelaschuk <teahelaschuk@gmail.com>
2017-09-10 15:32:47 -0700

    edited comments

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
+<!-- 
+COMP4711 Lab 1
+Site index
+-->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,8 +16,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include('Student.php');
-        
+        include('Student.php');        
         $students = array();
         
         // adding student #1
